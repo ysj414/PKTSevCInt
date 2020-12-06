cbb3f52608fb34f41fbbfd717911068b30379dde
ysj-arad <seon1474@gmail.com>
2020-12-06 16:30:45 +0900

    Makefile option add server function add.  - add log  - change one time service server -> iterative server

--- Makefile
+++ Makefile
@@ -4,16 +4,17 @@ CC=gcc
 server: server.o
 	rm -rf server 
 	${CC} -o server server.o
-server.o:
+server.o: server.c
 	${CC} -c server.c
 
 client: client.o
-	rm -rf client
+	rm -rf client 
 	${CC} -o client client.o
 
 client.o: client.c
 	${CC} -c client.c
 
 clean : 
-	rm -rf server client
+	@rm -rf server client server.o client.o
+	@echo "all files remove"
 
--- server.c
+++ server.c
@@ -14,6 +14,7 @@ int main(int argc, char* argv[])
 {
 	int serv_sock, c_sock;
 	char message[BUF_SIZE];
+	char buf[64];
 	int str_len, i;
 
 	struct sockaddr_in serv_adr;
@@ -25,37 +26,56 @@ int main(int argc, char* argv[])
 	if(serv_sock == -1)
 		error_handling("socket init error\n");
 	printf("socket create succuess\n");
-	
+
+/* Server init */	
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY); /* INADDR_ANY : 서버의 IP를 자동으로 찾아서 대입해주는 함수 */
 	serv_adr.sin_port = htons(atoi(argv[1]));
 
+
 	printf("Hello! I'am Server\n");
-	printf("My IP:%s Port: %s\n",inet_ntoa(serv_adr.sin_addr),argv[1]);
-                                
 	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1 )
 		error_handling("bind() error\n");
-	printf("bind success\n");
 
+/* Print Server Information.
+ * Server Port print correctly but IP address can't print.
+ * Later, this part will be fixed.
+ */
+
+
+/*  
+	inet_ntop(AF_INET,&serv_adr.sin_addr,sbuf,INET_ADDRSTRLEN);
+	printf("Port %d\n",ntohs(serv_adr.sin_port));
+	printf("Server Info : IP: %s\n",sbuf);
+*/
 	if(listen(serv_sock, 5) == -1 )
 		error_handling("listen() error\n");
-	printf("listen success\n");
-
-
-	
+	while(1)
+	{
 	c_sock = accept(serv_sock, (struct sockaddr *)&c_adr, &c_adr_sz);
+
 	if(c_sock == -1)
-		 error_handling("accept() error\n");
-	printf("Client Info : IP: %s, Port %d\n",inet_ntoa(c_adr.sin_addr),ntohs(c_adr.sin_port));
+	{
+		 printf("accept() error\n");
+		 continue;
+	}
+	inet_ntop(AF_INET,&c_adr.sin_addr,buf,INET_ADDRSTRLEN);
+	printf("Client has Connected!!\n");
+	printf("Client Info : IP: %s\n",buf);
 
-	while((str_len = read(c_sock, message, BUF_SIZE)) != 0)
+	while( (str_len = read(c_sock, message, BUF_SIZE)) != 0 )
+	{
+			printf("Server received data: %s",message);
+			printf("Transfer this data toward client again.\n");
 			write(c_sock, message, str_len);
-
+			memset(message,0,sizeof(message));
+	}
+	printf("Client has closed the connection\n");
+	printf("Server wait Client connection......\n");	
 	close(c_sock);
-
+	}
 	close(serv_sock);
-	
 
 	return 0;
 }
