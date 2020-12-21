5320bbef6e63a30a136895a7a210f3d300fb0f8e
ysj-arad <seon1474@gmail.com>
2020-12-21 19:13:16 +0900

    add config.h

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+/* server / client module management file */
+
+
+/* define calculator module */
+#define CAL 0
+
+/* define Three way handshake */
+#define THREEWAY_HANDSHAKE	1
+
+
+
+
+#endif /* __CONFIG_H__ */
